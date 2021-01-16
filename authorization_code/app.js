2dc745fce8fec6b70b8fe544b031fc10bf1ea125
navroze-a <45801358+navroze-a@users.noreply.github.com>
2021-01-16 18:11:34 -0500

    Update app.js
    Master main man dude

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -7,7 +7,8 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
-var express = require('express'); // Express web server framework
+const fetch = require('node-fetch');
+ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
@@ -18,6 +19,7 @@ var client_secret = 'd2cc59d25baf49ac94e939cbf23cb24a'; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 var access_token;
+
 
 /**
  * Generates a random string containing numbers and letters
@@ -145,12 +147,57 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-app.post('/search', (req, res) => {
-  console.log(req);
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/search', async(req, res) => {
+  //console.log(req);
   console.log("initiating a search");
   console.log(`access_token : ${access_token}`);
-  res.send("Hello World");
+  console.log(req.body.search);
+
+  let url = new URL("https://api.spotify.com/v1/search")
+  url.searchParams.append('q', req.body.search);
+  url.searchParams.append('type', 'track');
+  url.searchParams.append('limit', 1);
+
+ const topTrack = await fetch(url.href, {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json' , 'Authorization' : 'Bearer ' + access_token},
+    })
+
+  const topTrackJson = await topTrack.json();
+  console.log("id: " + topTrackJson.tracks.items[0].id);
+  console.log("name:" + topTrackJson.tracks.items[0].name);
+  let id = topTrackJson.tracks.items[0].id;
+  metrics(id, access_token);
 });
+
+
+async function metrics(id, access_token){
+  let url = new URL('https://api.spotify.com/v1/audio-features/' + id);
+
+  const attributes = await fetch(url.href, {
+    method: 'get',
+    headers: { 'Content-Type': 'application/json' , 'Authorization' : 'Bearer ' + access_token},
+})
+
+const attributesJson = await attributes.json();
+console.log(attributesJson);
+
+}
+
+async function produceLinkVector(attributes_json_format) {
+  let danceability = attributes_json_format.danceability;
+  let energy = attributes_json_format.energy;
+  let mode = attributes_json_format.mode;
+  let tempo = attributes_json_format.tempo;
+  let valence = attributes_json_format.valence;
+
+  // make vectors here!
+
+}
 
 
 console.log('Listening on 8888');
